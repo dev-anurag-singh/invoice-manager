8e3e0a6c53953feef7cc73f3bbf66c363511d06e
Anurag <dev.anuragkr@gmail.com>
2024-04-10 16:43:08 +0530

    changed dark mode background and avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 function Navbar() {
   return (
-    <header className='flex bg-carbon-blue justify-between lg:flex-col lg:rounded-r-3xl'>
+    <header className='flex bg-carbon-blue dark:bg-muted justify-between lg:flex-col lg:rounded-r-3xl'>
       <div>
         <Logo />
       </div>
@@ -12,7 +12,7 @@ function Navbar() {
         <Moon className=' fill-foreground-light cursor-pointer transition-colors hover:fill-white/70  stroke-inherit' />
         <div className='w-[1px] bg-[#494E6E] self-stretch lg:w-full lg:h-[1px]' />
         <Avatar>
-          <AvatarImage src='https://github.com/shadcn.png' />
+          <AvatarImage src='https://github.com/dev-anurag-singh.png' />
           <AvatarFallback>CN</AvatarFallback>
         </Avatar>
       </div>
