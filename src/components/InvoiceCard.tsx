1fb1c2250c99b09012e0b8f4258b00405a7dc5c5
Anurag <dev.anuragkr@gmail.com>
2024-04-12 16:50:55 +0530

    updated invoice card to show real invoice data

--- src/components/InvoiceCard.tsx
+++ src/components/InvoiceCard.tsx
@@ -1,37 +1,62 @@
 import { ChevronRight, Dot } from 'lucide-react';
 import { Card, CardContent } from './ui/card';
+import moment from 'moment';
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
-function InvoiceCard() {
+interface InvoiceCardProps {
+  invoice: {
+    id: string;
+    paymentDue: string;
+    status: string;
+    total: number;
+    clientName: string;
+  };
+}
+
+function InvoiceCard({ invoice }: InvoiceCardProps) {
   return (
-    <Card className='hover:border-primary hover:shadow-md group cursor-pointer transition-all'>
-      <CardContent className='pt-6'>
-        <div className='space-y-6 relative md:flex md:justify-between md:space-y-0 md:items-center'>
-          <div className='flex justify-between'>
-            <div className='text-sm md:mr-6'>
-              <span className=''>#</span>
-              <span className='text-foreground'>RT3080</span>
+    <Link href={`/invoices/${invoice.id}`} className='block'>
+      <Card className='hover:border-primary hover:shadow-md group transition-all'>
+        <CardContent className='pt-6'>
+          <div className='space-y-6 md:space-y-0 relative md:grid md:grid-cols-[4fr_3fr] md:items-center md:gap-2'>
+            <div className='flex justify-between md:grid md:grid-cols-[2fr_3fr_3fr]'>
+              <div className='text-sm'>
+                <span className=''>#</span>
+                <span className='text-foreground'>{invoice.id}</span>
+              </div>
+              <div className='absolute md:static bottom-8 left-0 text-xs space-x-[6px]'>
+                <span className='text-foreground-light'>Due</span>
+                <span>{moment(invoice.paymentDue).format('ll')}</span>
+              </div>
+              <div>
+                <p className='text-xs'>{invoice.clientName}</p>
+              </div>
             </div>
-            <div className='absolute md:static md:mr-12 bottom-8 left-0 text-xs space-x-[6px]'>
-              <span className='text-foreground-light'>Due</span>
-              <span>19 Aug 2021</span>
-            </div>
-            <div>
-              <p className='text-xs'>Jensen Huang</p>
+            <div className='flex justify-between items-end md:items-center md:grid md:grid-cols-[2fr_2fr_1fr] md:gap-3'>
+              <p className='text-md text-foreground'>$ {invoice.total}</p>
+              <div
+                className={cn(
+                  'pt-[14px] pb-[11px] w-[6.5rem] bg-carbon-blue/5 text-carbon-blue dark:bg-grey/5 dark:text-[#DFE3FA]  rounded-sm flex items-center justify-center',
+                  {
+                    'bg-green/5 text-green': invoice.status === 'paid',
+                    'bg-orange/5 text-orange': invoice.status === 'pending',
+                  }
+                )}
+              >
+                <Dot className='stroke-[8px]' />
+                <p className='text-sm pt-[3px] pr-[6px] capitalize'>
+                  {invoice.status}
+                </p>
+              </div>
+              <div className='hidden md:block cursor-pointer pl-2'>
+                <ChevronRight className='h-4 w-4 group-hover:translate-x-1 transition-transform' />
+              </div>
             </div>
           </div>
-          <div className='flex justify-between items-end md:items-center'>
-            <p className='text-md text-foreground'>$ 1,800</p>
-            <div className='pl-6 md:ml-10 md:mr-5 pr-7 pt-[14px] pb-[11px] bg-green/5 rounded-sm flex items-center'>
-              <Dot className='stroke-[8px] stroke-green' />
-              <p className='text-sm text-green mt-[2px] '>Paid</p>
-            </div>
-            <div className='hidden md:block cursor-pointer'>
-              <ChevronRight className='h-4 w-4 group-hover:translate-x-1 transition-transform' />
-            </div>
-          </div>
-        </div>
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+    </Link>
   );
 }
 
