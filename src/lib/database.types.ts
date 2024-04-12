8dbb1f32e80883455e908e8b983e739d36894e75
Anurag <dev.anuragkr@gmail.com>
2024-04-12 16:51:29 +0530

    database types comming from supabase

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -4,224 +4,224 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[]
+  | Json[];
 
 export type Database = {
   public: {
     Tables: {
       addresses: {
         Row: {
-          city: string
-          country: string
-          created_at: string
-          id: number
-          post: string
-          street: string
-        }
+          city: string;
+          country: string;
+          created_at: string;
+          id: number;
+          post: string;
+          street: string;
+        };
         Insert: {
-          city: string
-          country: string
-          created_at?: string
-          id?: number
-          post: string
-          street: string
-        }
+          city: string;
+          country: string;
+          created_at?: string;
+          id?: number;
+          post: string;
+          street: string;
+        };
         Update: {
-          city?: string
-          country?: string
-          created_at?: string
-          id?: number
-          post?: string
-          street?: string
-        }
-        Relationships: []
-      }
+          city?: string;
+          country?: string;
+          created_at?: string;
+          id?: number;
+          post?: string;
+          street?: string;
+        };
+        Relationships: [];
+      };
       invoices: {
         Row: {
-          clientAddress: number
-          clientEmail: string
-          clientName: string
-          created_at: string
-          description: string
-          id: number
-          issueDate: string
-          paymentDue: string
-          paymentTerms: number
-          senderAddress: number
-          status: string
-        }
+          clientAddress: number;
+          clientEmail: string;
+          clientName: string;
+          created_at: string;
+          description: string;
+          id: number;
+          issueDate: string;
+          paymentDue: string;
+          paymentTerms: number;
+          senderAddress: number;
+          status: string;
+        };
         Insert: {
-          clientAddress: number
-          clientEmail: string
-          clientName: string
-          created_at?: string
-          description: string
-          id?: number
-          issueDate: string
-          paymentDue: string
-          paymentTerms: number
-          senderAddress: number
-          status: string
-        }
+          clientAddress: number;
+          clientEmail: string;
+          clientName: string;
+          created_at?: string;
+          description: string;
+          id?: number;
+          issueDate: string;
+          paymentDue: string;
+          paymentTerms: number;
+          senderAddress: number;
+          status: string;
+        };
         Update: {
-          clientAddress?: number
-          clientEmail?: string
-          clientName?: string
-          created_at?: string
-          description?: string
-          id?: number
-          issueDate?: string
-          paymentDue?: string
-          paymentTerms?: number
-          senderAddress?: number
-          status?: string
-        }
+          clientAddress?: number;
+          clientEmail?: string;
+          clientName?: string;
+          created_at?: string;
+          description?: string;
+          id?: number;
+          issueDate?: string;
+          paymentDue?: string;
+          paymentTerms?: number;
+          senderAddress?: number;
+          status?: string;
+        };
         Relationships: [
           {
-            foreignKeyName: "public_invoices_clientAddress_fkey"
-            columns: ["clientAddress"]
-            isOneToOne: false
-            referencedRelation: "addresses"
-            referencedColumns: ["id"]
+            foreignKeyName: 'public_invoices_clientAddress_fkey';
+            columns: ['clientAddress'];
+            isOneToOne: false;
+            referencedRelation: 'addresses';
+            referencedColumns: ['id'];
           },
           {
-            foreignKeyName: "public_invoices_senderAddress_fkey"
-            columns: ["senderAddress"]
-            isOneToOne: false
-            referencedRelation: "addresses"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
+            foreignKeyName: 'public_invoices_senderAddress_fkey';
+            columns: ['senderAddress'];
+            isOneToOne: false;
+            referencedRelation: 'addresses';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
       items: {
         Row: {
-          created_at: string
-          id: number
-          invoice_id: number
-          name: string
-          price: number
-          quantity: number
-        }
+          created_at: string;
+          id: number;
+          invoice_id: number;
+          name: string;
+          price: number;
+          quantity: number;
+        };
         Insert: {
-          created_at?: string
-          id?: number
-          invoice_id: number
-          name: string
-          price: number
-          quantity: number
-        }
+          created_at?: string;
+          id?: number;
+          invoice_id: number;
+          name: string;
+          price: number;
+          quantity: number;
+        };
         Update: {
-          created_at?: string
-          id?: number
-          invoice_id?: number
-          name?: string
-          price?: number
-          quantity?: number
-        }
+          created_at?: string;
+          id?: number;
+          invoice_id?: number;
+          name?: string;
+          price?: number;
+          quantity?: number;
+        };
         Relationships: [
           {
-            foreignKeyName: "public_items_invoice_id_fkey"
-            columns: ["invoice_id"]
-            isOneToOne: false
-            referencedRelation: "invoices"
-            referencedColumns: ["id"]
-          },
-        ]
-      }
-    }
+            foreignKeyName: 'public_items_invoice_id_fkey';
+            columns: ['invoice_id'];
+            isOneToOne: false;
+            referencedRelation: 'invoices';
+            referencedColumns: ['id'];
+          }
+        ];
+      };
+    };
     Views: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Functions: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     Enums: {
-      [_ in never]: never
-    }
+      [_ in never]: never;
+    };
     CompositeTypes: {
-      [_ in never]: never
-    }
-  }
-}
+      [_ in never]: never;
+    };
+  };
+};
 
-type PublicSchema = Database[Extract<keyof Database, "public">]
+type PublicSchema = Database[Extract<keyof Database, 'public'>];
 
 export type Tables<
   PublicTableNameOrOptions extends
-    | keyof (PublicSchema["Tables"] & PublicSchema["Views"])
+    | keyof (PublicSchema['Tables'] & PublicSchema['Views'])
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-        Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    ? keyof (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+        Database[PublicTableNameOrOptions['schema']]['Views'])
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
-      Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R
+  ? (Database[PublicTableNameOrOptions['schema']]['Tables'] &
+      Database[PublicTableNameOrOptions['schema']]['Views'])[TableName] extends {
+      Row: infer R;
     }
     ? R
     : never
-  : PublicTableNameOrOptions extends keyof (PublicSchema["Tables"] &
-        PublicSchema["Views"])
-    ? (PublicSchema["Tables"] &
-        PublicSchema["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R
-      }
-      ? R
-      : never
+  : PublicTableNameOrOptions extends keyof (PublicSchema['Tables'] &
+      PublicSchema['Views'])
+  ? (PublicSchema['Tables'] &
+      PublicSchema['Views'])[PublicTableNameOrOptions] extends {
+      Row: infer R;
+    }
+    ? R
     : never
+  : never;
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+    | keyof PublicSchema['Tables']
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Insert: infer I;
     }
     ? I
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I
-      }
-      ? I
-      : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+      Insert: infer I;
+    }
+    ? I
     : never
+  : never;
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
-    | keyof PublicSchema["Tables"]
+    | keyof PublicSchema['Tables']
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    ? keyof Database[PublicTableNameOrOptions['schema']]['Tables']
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U
+  ? Database[PublicTableNameOrOptions['schema']]['Tables'][TableName] extends {
+      Update: infer U;
     }
     ? U
     : never
-  : PublicTableNameOrOptions extends keyof PublicSchema["Tables"]
-    ? PublicSchema["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U
-      }
-      ? U
-      : never
+  : PublicTableNameOrOptions extends keyof PublicSchema['Tables']
+  ? PublicSchema['Tables'][PublicTableNameOrOptions] extends {
+      Update: infer U;
+    }
+    ? U
     : never
+  : never;
 
 export type Enums<
   PublicEnumNameOrOptions extends
-    | keyof PublicSchema["Enums"]
+    | keyof PublicSchema['Enums']
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
-    ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    ? keyof Database[PublicEnumNameOrOptions['schema']]['Enums']
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
-  ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
-  : PublicEnumNameOrOptions extends keyof PublicSchema["Enums"]
-    ? PublicSchema["Enums"][PublicEnumNameOrOptions]
-    : never
+  ? Database[PublicEnumNameOrOptions['schema']]['Enums'][EnumName]
+  : PublicEnumNameOrOptions extends keyof PublicSchema['Enums']
+  ? PublicSchema['Enums'][PublicEnumNameOrOptions]
+  : never;
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,8 +3,4 @@ import { Tables } from './database.types';
 export type Address = Tables<'addresses'>;
 export type Item = Tables<'items'>;
 
-export type Invoice = Tables<'invoices'> & {
-  items: Item;
-  senderAddress: Address;
-  clientAddress: Address;
-};
+export type Invoice = Tables<'invoices'>;
