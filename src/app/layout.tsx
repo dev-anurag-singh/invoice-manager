2d3e809323276781d3bbd865d462117c67c3fb04
Anurag <dev.anuragkr@gmail.com>
2024-04-15 21:13:28 +0530

    dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' className='dark'>
+    <html lang='en' className=''>
       <body className={cn(font.className)}>
         <div className='flex flex-col h-screen lg:flex-row'>
           <Navbar />
