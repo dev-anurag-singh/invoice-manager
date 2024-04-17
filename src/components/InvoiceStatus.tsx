bf74e2c24271825cc203df5cc1300f201b839e30
Anurag <dev.anuragkr@gmail.com>
2024-04-17 22:18:00 +0530

    invoice status component

---
+++ src/components/InvoiceStatus.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/utils';
+import { Dot } from 'lucide-react';
+
+function InvoiceStatus({ status }: { status: string }) {
+  return (
+    <div
+      className={cn(
+        'pt-[14px] pb-[11px] w-[6.5rem] bg-carbon-blue/5 text-carbon-blue dark:bg-grey/5 dark:text-[#DFE3FA]  rounded-md flex items-center justify-center',
+        {
+          'bg-green/5 text-green': status === 'paid',
+          'bg-orange/5 text-orange': status === 'pending',
+        }
+      )}
+    >
+      <Dot className='stroke-[8px]' />
+      <p className='text-sm pt-[3px] pr-[6px] capitalize'>{status}</p>
+    </div>
+  );
+}
+
+export default InvoiceStatus;
