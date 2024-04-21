90c745e28fac06764cb4efd0a0ca33cdb95cf3db
Anurag <dev.anuragkr@gmail.com>
2024-04-21 21:38:12 +0530

    updated bg-color and text-color

--- src/components/InvoiceStatus.tsx
+++ src/components/InvoiceStatus.tsx
@@ -5,10 +5,12 @@ function InvoiceStatus({ status }: { status: string }) {
   return (
     <div
       className={cn(
-        'pt-[14px] pb-[11px] w-[6.5rem] bg-carbon-blue/5 text-carbon-blue dark:bg-grey/5 dark:text-[#DFE3FA]  rounded-md flex items-center justify-center',
+        'pt-[14px] pb-[11px] w-[6.5rem] bg-carbon-blue/5 text-carbon-blue  rounded-md flex items-center justify-center',
         {
           'bg-green/5 text-green': status === 'paid',
           'bg-orange/5 text-orange': status === 'pending',
+          'bg-carbon-blue/5 text-carbon-blue dark:bg-grey/5 dark:text-[#DFE3FA]':
+            status === 'draft',
         }
       )}
     >
