9edd1e9575bc0f4f73fb1f7aabf2d95dbd8baed4
Anurag <dev.anuragkr@gmail.com>
2024-04-26 00:35:49 +0530

    updated Invoice card grid layout

--- src/components/InvoiceCard.tsx
+++ src/components/InvoiceCard.tsx
@@ -19,11 +19,11 @@ function InvoiceCard({ invoice }: InvoiceCardProps) {
   return (
     <Link href={`/invoices/${invoice.id}`} className='block'>
       <Card className='hover:border-primary hover:shadow-md group transition-all'>
-        <CardContent className='pt-6 md:py-4'>
-          <div className='space-y-6 md:space-y-0 relative md:grid md:grid-cols-[4fr_3fr] md:items-center md:gap-10'>
+        <CardContent className='pt-6 md:py-4 lg:px-8'>
+          <div className='space-y-6 md:space-y-0 relative md:grid md:grid-cols-[1fr_16.5rem] md:items-center '>
             <div className='flex justify-between md:grid md:grid-cols-[2fr_3fr_3fr]'>
               <div className='text-sm'>
-                <span className=''>#</span>
+                <span className='mr-[1px]'>#</span>
                 <span className='text-foreground'>{invoice.id}</span>
               </div>
               <div className='absolute md:static bottom-8 left-0 text-xs space-x-[6px]'>
@@ -34,10 +34,12 @@ function InvoiceCard({ invoice }: InvoiceCardProps) {
                 <p className='text-xs'>{invoice.clientName}</p>
               </div>
             </div>
-            <div className='flex justify-between items-end md:items-center md:grid md:grid-cols-[2fr_2fr_1fr] md:gap-3'>
-              <p className='text-md text-foreground'>$ {invoice.total}</p>
+            <div className='flex justify-between items-end md:items-center md:grid md:grid-cols-[1fr_6.5rem_1rem] md:gap-5'>
+              <p className='text-md text-foreground md:text-right pr-5'>
+                $ {invoice.total}
+              </p>
               <InvoiceStatus status={invoice.status} />
-              <div className='hidden md:block cursor-pointer pl-2'>
+              <div className='hidden md:block'>
                 <ChevronRight className='h-4 w-4 stroke-[3px] group-hover:translate-x-1 stroke-primary transition-transform' />
               </div>
             </div>
