cae4d4965f46537b6e9b57e88597d4cd3b4e6afd
Anurag <dev.anuragkr@gmail.com>
2024-05-06 23:43:54 +0530

    minor updates in styles

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -12,7 +12,7 @@ export type CalendarProps = React.ComponentProps<typeof DayPicker>;
 function Calendar({
   className,
   classNames,
-  showOutsideDays = true,
+  showOutsideDays = false,
   ...props
 }: CalendarProps) {
   return (
@@ -39,7 +39,7 @@ function Calendar({
         cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
           buttonVariants({ variant: 'ghost', size: 'small' }),
-          'aria-selected:opacity-100 rounded-md'
+          'aria-selected:opacity-100 rounded-md hover:text-primary'
         ),
         day_range_end: 'day-range-end',
         day_selected:
