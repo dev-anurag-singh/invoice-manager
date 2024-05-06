04ae4e0f3d405cb4d7534460dc7544d42c1fbca5
Anurag <dev.anuragkr@gmail.com>
2024-05-06 22:58:24 +0530

    removed some styles

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -39,7 +39,7 @@ function Calendar({
         cell: 'h-9 w-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
         day: cn(
           buttonVariants({ variant: 'ghost', size: 'small' }),
-          'aria-selected:opacity-100 rounded-md hover:bg-primary/20 hover:text-foreground'
+          'aria-selected:opacity-100 rounded-md'
         ),
         day_range_end: 'day-range-end',
         day_selected:
