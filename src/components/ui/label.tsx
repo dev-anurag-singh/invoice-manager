fc0849f7cd7c634ca729449b86e62e3e3d7bf2a5
Anurag <dev.anuragkr@gmail.com>
2024-05-15 23:56:38 +0530

    removed text-color

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import { cn } from '@/lib/utils';
 
 const labelVariants = cva(
-  'text-xs text-foreground-light inline-block peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+  'text-xs inline-block peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
 );
 
 const Label = React.forwardRef<
