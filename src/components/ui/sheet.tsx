9367075c8674c553ab14a9addf616ec9cd71b837
Anurag <dev.anuragkr@gmail.com>
2024-05-17 23:44:14 +0530

    changed default padding

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -31,7 +31,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName;
 
 const sheetVariants = cva(
-  'absolute z-50 gap-4 bg-background text-foreground p-6 pt-8 md:p-14 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
+  'absolute z-50 gap-4 bg-muted text-muted-foreground p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500',
   {
     variants: {
       side: {
