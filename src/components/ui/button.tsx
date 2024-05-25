f9390bc40fe72f3440cca2775a11948211088904
Anurag <dev.anuragkr@gmail.com>
2024-05-25 23:38:17 +0530

    added tertiary button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -16,6 +16,7 @@ const buttonVariants = cva(
           'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary-hover hover:text-foreground-light',
+        tertiary: 'bg-tertiary text-tertiary-foreground hover:bg-tertiary/90',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
