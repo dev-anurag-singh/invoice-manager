e070ca5c579efb0d13e1589f23bbb95b717580b4
Anurag <dev.anuragkr@gmail.com>
2024-06-21 16:47:36 +0530

    Filter option component

---
+++ src/components/FilterOption.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useId } from 'react';
+import { Checkbox } from './ui/checkbox';
+
+interface FilterOptionProps {
+  label: string;
+  checked: boolean;
+  handleFilter: (append: boolean, term: string) => void;
+}
+
+function FilterOption({ label, checked, handleFilter }: FilterOptionProps) {
+  const id = useId();
+
+  return (
+    <div className='flex items-center space-x-3'>
+      <Checkbox
+        defaultChecked={checked}
+        onCheckedChange={state => {
+          if (state === true) handleFilter(true, label);
+          if (state === false) handleFilter(false, label);
+        }}
+        id={id}
+      />
+      <label
+        htmlFor={id}
+        className='text-sm capitalize cursor-pointer peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+      >
+        {label}
+      </label>
+    </div>
+  );
+}
+
+export default FilterOption;
