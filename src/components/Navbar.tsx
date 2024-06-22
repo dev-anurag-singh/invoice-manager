1f2f44c38dda2b30e94968b23f126b54c5f38da5
Anurag <dev.anuragkr@gmail.com>
2024-06-22 23:53:15 +0530

    changes in Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,14 @@
+import Link from 'next/link';
 import Logo from './Logo';
 import { ThemeToggler } from './ThemeToggler';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 function Navbar() {
   return (
-    <header className='flex bg-carbon-blue dark:bg-muted justify-between lg:flex-col lg:rounded-r-3xl'>
-      <div>
+    <header className='fixed top-0 left-0 w-full z-50 lg:h-full lg:w-auto flex shadow-sm bg-carbon-blue dark:bg-muted justify-between lg:flex-col lg:rounded-r-3xl'>
+      <Link href={'/'}>
         <Logo />
-      </div>
+      </Link>
       <div className='pr-6 flex items-center gap-6 lg:flex-col lg:pr-0 lg:pb-6'>
         <ThemeToggler />
         <div className='w-[1px] bg-[#494E6E] self-stretch lg:w-full lg:h-[1px]' />
