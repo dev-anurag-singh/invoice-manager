d83f7db1785875d6875f0764761f82459f81844c
Anurag <dev.anuragkr@gmail.com>
2024-07-10 23:40:19 +0530

    full screen loading component

---
+++ src/components/Loading.tsx
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="grid h-full place-content-center">
+      <div className="h-16 w-16 animate-spin rounded-full border-8 border-dashed border-primary" />
+    </div>
+  );
+}
+
+export default Loading;
