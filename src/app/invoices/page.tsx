b82f66409fce6bd1ec3fbec7494521f62a0b0858
Anurag <dev.anuragkr@gmail.com>
2024-07-11 18:53:01 +0530

    inplemented filter for invoices

--- src/app/invoices/page.tsx
+++ src/app/invoices/page.tsx
@@ -7,12 +7,39 @@ import { Suspense } from "react";
 import { useInvoice } from "@/components/InvoiceContext";
 import { useIsMounted } from "../hooks/useIsMounted";
 import Loading from "@/components/Loading";
+import { useSearchParams } from "next/navigation";
+import { TInvoice } from "@/lib/types";
 
 export default function Home() {
   const { invoices } = useInvoice();
   const [isMounted] = useIsMounted();
+  const searchParams = useSearchParams();
+  const filterParams = searchParams.getAll("filter");
 
   if (!isMounted || !invoices) return <Loading />;
+
+  let filteredInvoices: TInvoice[] = [];
+
+  if (
+    filterParams.includes("paid") ||
+    filterParams.includes("pending") ||
+    filterParams.includes("draft")
+  ) {
+    if (filterParams.includes("paid")) {
+      const paidInvoices = invoices.filter((i) => i.status === "paid");
+      filteredInvoices.push(...paidInvoices);
+    }
+    if (filterParams.includes("pending")) {
+      const pendingInvoices = invoices.filter((i) => i.status === "pending");
+      filteredInvoices.push(...pendingInvoices);
+    }
+    if (filterParams.includes("draft")) {
+      const drafts = invoices.filter((i) => i.status === "draft");
+      filteredInvoices.push(...drafts);
+    }
+  } else {
+    filteredInvoices = invoices;
+  }
 
   return (
     <div className="container">
@@ -20,7 +47,7 @@ export default function Home() {
         <div className="mr-auto flex flex-col">
           <h2 className="text-lg md:text-xl">Invoices</h2>
           <span className="text-xs text-foreground-light">
-            {invoices.length} invoices
+            {filteredInvoices.length} invoices
           </span>
         </div>
         <Suspense>
@@ -28,9 +55,9 @@ export default function Home() {
         </Suspense>
         <CreateInvoice />
       </div>
-      {invoices.length ? (
+      {filteredInvoices.length ? (
         <div className="space-y-4 pb-8">
-          {invoices.map((invoice) => (
+          {filteredInvoices.map((invoice) => (
             <InvoiceCard key={invoice.id} invoice={invoice} />
           ))}
         </div>
