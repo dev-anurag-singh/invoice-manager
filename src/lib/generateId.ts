f8621b945367291f4df5ed07287f8a94f7eb9b5b
Anurag <dev.anuragkr@gmail.com>
2024-07-21 23:41:49 +0530

    updated generateId function

--- src/lib/generateId.ts
+++ src/lib/generateId.ts
@@ -1,6 +1,6 @@
 export const generateId = () => {
   const alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const randomIndex = Math.floor(Math.random() * alphabets.length);
-  const timestamp = Date.now();
+  const timestamp = Date.now().toString().slice(-5);
   return `${alphabets[randomIndex]}${timestamp}`;
 };
