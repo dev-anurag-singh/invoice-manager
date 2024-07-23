a8d4aed176b1a2fcd690c6bae875de5fafff672f
Anurag <dev.anuragkr@gmail.com>
2024-07-23 15:37:48 +0530

    added update invoice function

--- src/components/InvoiceContext.tsx
+++ src/components/InvoiceContext.tsx
@@ -2,13 +2,16 @@ import { TInvoice } from "@/lib/types";
 import { createContext, useContext, useEffect } from "react";
 import { useLocalStorage } from "usehooks-ts";
 import data from "@/data.json";
-import Loading from "./Loading";
+import { generateId } from "@/lib/generateId";
+import { TFormSchema as InvoiceFormSchema } from "./InvoiceForm";
+import moment from "moment";
 
 interface ContextType {
   invoices: TInvoice[] | null;
   deleteInvoice: (id: string) => void;
   markAsPaid: (id: string) => void;
-  createInvoice: (data: Partial<TInvoice>) => void;
+  createInvoice: (data: InvoiceFormSchema) => TInvoice;
+  updateInvoice: (id: string, data: InvoiceFormSchema) => TInvoice;
 }
 
 const InvoiceContext = createContext<ContextType>({} as ContextType);
@@ -21,23 +24,108 @@ function InvoiceProvider({ children }: { children: React.ReactNode }) {
 
   useEffect(() => {
     if (!value) {
+      // @ts-ignore
       setValue(data);
     }
   }, [setValue, value]);
 
   // FUNCTION TO ADD A NEW INVOICE
-  function createInvoice(data: Partial<TInvoice>) {
-    console.log(data);
+  function createInvoice(data: InvoiceFormSchema) {
+    const id = generateId();
+    const invoiceDate = moment(data.invoiceDate).format().split("T")[0];
+    const paymentDue = moment(data.invoiceDate)
+      .add(data.paymentTerm, "days")
+      .format()
+      .split("T")[0];
+    let invoiceTotal = 0;
+
+    const items = data.items.map(({ name, price, quantity }) => {
+      const total = price * quantity;
+
+      invoiceTotal = invoiceTotal + total;
+
+      return {
+        name,
+        price: Number(price),
+        quantity: Number(quantity),
+        total,
+      };
+    });
+
+    const invoice = {
+      id,
+      senderAddress: data.from,
+      clientAddress: data.to,
+      clientEmail: data.clientEmail,
+      clientName: data.clientName,
+      description: data.description,
+      invoiceDate,
+      paymentDue,
+      items,
+      paymentTerm: data.paymentTerm,
+      status: data.status,
+      total: invoiceTotal,
+    };
+
+    setValue([invoice, ...value!]);
+
+    return invoice;
+  }
+
+  // FUNCTION TO UPDATE AN INVOICE
+  function updateInvoice(id: string, data: InvoiceFormSchema) {
+    const invoices = value?.filter((i) => i.id !== id) || [];
+
+    const invoiceDate = moment(data.invoiceDate).format().split("T")[0];
+    const paymentDue = moment(data.invoiceDate)
+      .add(data.paymentTerm, "days")
+      .format()
+      .split("T")[0];
+    let invoiceTotal = 0;
+
+    const items = data.items.map(({ name, price, quantity }) => {
+      const total = price * quantity;
+
+      invoiceTotal = invoiceTotal + total;
+
+      return {
+        name,
+        price: Number(price),
+        quantity: Number(quantity),
+        total,
+      };
+    });
+
+    const invoice = {
+      id,
+      senderAddress: data.from,
+      clientAddress: data.to,
+      clientEmail: data.clientEmail,
+      clientName: data.clientName,
+      description: data.description,
+      invoiceDate,
+      paymentDue,
+      items,
+      paymentTerm: data.paymentTerm,
+      status: data.status,
+      total: invoiceTotal,
+    };
+
+    setValue([invoice, ...invoices]);
+
+    return invoice;
   }
 
   // FUNTION TO DELETE AN INVOICE
   function deleteInvoice(id: string) {
     if (!value) return;
     const filteredInvoices = value.filter((invoice) => invoice.id !== id);
-    setValue(filteredInvoices);
+    setTimeout(() => {
+      setValue(filteredInvoices);
+    }, 2000);
   }
 
-  // FUNCTION TO UPDATE AN INVOICE
+  // FUNCTION TO MARK AN INVOICE AS PAID
   function markAsPaid(id: string) {
     if (!value) return;
 
@@ -51,7 +139,13 @@ function InvoiceProvider({ children }: { children: React.ReactNode }) {
 
   return (
     <InvoiceContext.Provider
-      value={{ invoices: value, deleteInvoice, markAsPaid, createInvoice }}
+      value={{
+        invoices: value,
+        deleteInvoice,
+        markAsPaid,
+        createInvoice,
+        updateInvoice,
+      }}
     >
       {children}
     </InvoiceContext.Provider>
